1f9cbcda96e45b6560f08d6b1d3518c98eb9371d
Tay Starr <tstarrroberts@gmail.com>
2022-05-03 15:55:38 -0400

    add candidate query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,9 +8,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // default MySQL username is 'root'
+        user: 'root',
+        password: ' ',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 // default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 
